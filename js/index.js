13136d0ce4b604efd5e7115a7f50b7332ebf6e28
Alberto Hrtzt <me@albertohartzet.com>
2018-03-15 20:32:46 -0600

    Intento

--- js/index.js
+++ js/index.js
@@ -1,25 +1,45 @@
 
 $('.slider').slick({
     autoplay: false,
-    autoplaySpeed: 5000,
+    autoplaySpeed: 3000,
     infinite: true,
-    speed: 3000,
+    speed: 500,
     fade: true,
     cssEase: 'linear',
     useCSS: true,
     swipe: false,
     touchMove: false,
-    arrows: true
-});
+    arrows: true,
+    useCSS: false,
+    useTransform: false,
+    waitForAnimate: false,
+}).slick("pause");
 
 $('.slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
-    $('.slick-slide').animate({'opacity': '1' }, 0);
+    //e.preventDefault();
+    //setTimeout(function(){
+    //    $('.slider').slickPlay();
+    //}),3000;
+    //$('.slick-slide').animate({
+    //    'opacity': '1'
+    //}, 0);
     $('.slick-active').addClass('slide-out');
-	$(this).addClass('is-changing');
+    $(this).addClass('is-changing');
 });
-
+//
 $('.slider').on('afterChange', function(event, slick, currentSlide){
-	$('.slick-slide').animate({'opacity': '1' }, 200);
+	//$('.slick-slide').animate({
+    //    'opacity': '1'
+    //}, 200);
     $('.slick-slide').removeClass('slide-out');
 	$(this).removeClass('is-changing');
 });
+
+
+// var initialDelay = 3000;
+//
+// $(document).one("mousemove touchstart", function() {
+//     setTimeout(function() {
+//         $(".slider").slick("play");
+//     },initialDelay);
+// });
